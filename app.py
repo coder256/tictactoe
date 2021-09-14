865cc7d1a4b1670c4c3c17423d3af0812e0b05c9
Andrew M <mamugasa@gmail.com>
2021-09-14 14:39:54 +0300

    can play empty/winning/blocking moves

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, json, request, make_response
 import math
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -8,8 +9,8 @@ app = Flask(__name__)
 def index():
     # board = "xoxoxo x "
     board = request.args.get("board")
+    result = board
     if board is not None:
-        print(f"board position 4:: {board[4]}")
         if isValidBoard(board) is False:
             return invalidBoard()
 
@@ -19,8 +20,14 @@ def index():
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [2, 4, 6]
         ]
+        # check if board is empty
+        if board.count(" ") == 9:
+            result = play(board, 0)
+
         wp = 0
         bp = 0
+        wAxis = []
+        bAxis = []
         for axis in axes:
             print(f"the axis:: {axis}")
             # check for playable space
@@ -29,16 +36,35 @@ def index():
             if axis_string.count(" ") > 0:
                 if isWinningPosition(axis_string):
                     print(f"winning position on:: {axis}")
+                    wAxis = axis
                     wp += 1
                 if isBlockablePosition(axis_string):
                     print(f"blockable position on:: {axis}")
+                    bAxis = axis
                     bp += 1
             else:
                 print("no playable space on this axis: ", axis)
 
         print(f"wp: {wp} --- bp: {bp}")
+        print(f"wAxis: {wAxis} --- bAxis: {bAxis}")
 
-        return f"valid board :: {isValidBoard(board)}"
+        # play
+        # play winning position
+        if wp > 0:
+            for position in wAxis:
+                if board[position] == " ":
+                    print("playable winning position ::", position)
+                    result = play(board, position)
+
+        # play blocking move
+        if bp > 0:
+            for position in bAxis:
+                if board[position] == " ":
+                    print("playable blockable position ::", position)
+                    result = play(board, position)
+
+        return f"valid board :: {urllib.parse.quote_plus(result)}"
+        # return f"valid board :: {result}"
     else:
         return invalidBoard()
 
@@ -57,8 +83,9 @@ def isValidBoard(board):
             return False
 
     # make sure o can play next
-    if math.fabs(board.count("o") - board.count("x")) > 1:
-        diff = math.fabs(board.count("o") - board.count("x"))
+    diff = board.count("o") - board.count("x")
+    if diff != 0 and diff != -1:
+        # diff = math.fabs(board.count("o") - board.count("x"))
         print(f"illegal moves played :: {diff}")
         return False
 
@@ -72,8 +99,8 @@ def isValidBoard(board):
     for axis in axes:
         # check for playable space
         axis_string = f"{board[axis[0]]}{board[axis[1]]}{board[axis[2]]}"
-        if axis_string.count("x") == 3:
-            print(f"x already won on {axis}")
+        if axis_string.count("x") == 3 or axis_string.count("o") == 3:
+            print(f"already won on {axis}")
             return False
 
     return True
@@ -91,3 +118,7 @@ def isWinningPosition(axisString):
 def isBlockablePosition(axisString):
     if axisString.count("x") == 2:
         return True
+
+
+def play(board, position):
+    return board[:position] + "o" + board[position+1:]
